496da830556ff7a33ddb344ee345a817245224ee
airlabcmu on dtc omen <airlabcmu@gmail.com>
2026-08-11 12:30:48 -0400

    Add apply_pamr and use_fp16 config options

--- physics_atv_visual_mapping/image_processing/processing_blocks/talk2dino_seg.py
+++ physics_atv_visual_mapping/image_processing/processing_blocks/talk2dino_seg.py
@@ -14,12 +14,14 @@ class Talk2DinoSegBlock(ImageProcessingBlock):
     """
     Perform semantic segmentation with Talk2Dino (Barselotti et al. 2025)
     """
-    def __init__(self, ontology, image_insize, sharpness, return_logits, models_dir, device='cuda'):
+    def __init__(self, ontology, image_insize, sharpness, return_logits, models_dir, device='cuda', apply_pamr=True, use_fp16=False):
         self.ontology = load_ontology(ontology)
         self.image_insize = image_insize
         self.sharpness = sharpness
         self.return_logits = return_logits
         self.device = device
+        self.apply_pamr = apply_pamr
+        self.use_fp16 = use_fp16
 
         ##setup talk2dino
         self.talk2dino = AutoModel.from_pretrained(
@@ -37,15 +39,27 @@ class Talk2DinoSegBlock(ImageProcessingBlock):
         image_resize = F.resize(image, self.image_insize)
         image_in = image_resize[:, [2,1,0]] * 255. #1-scaled BGR -> 255-scaled RGB
 
-        masks, _ = self.talk2dino.generate_masks(
-            image_in,
-            img_metas = None,
-            text_emb = self.text_embed,
-            classnames = ' '.join(self.ontology['labels']),
-            apply_pamr = True
-        )
+        use_amp = self.use_fp16 and str(self.device).startswith('cuda')
+        with torch.inference_mode():
+            if use_amp:
+                with torch.autocast(device_type='cuda', dtype=torch.float16):
+                    masks, _ = self.talk2dino.generate_masks(
+                        image_in,
+                        img_metas = None,
+                        text_emb = self.text_embed,
+                        classnames = ' '.join(self.ontology['labels']),
+                        apply_pamr = self.apply_pamr
+                    )
+            else:
+                masks, _ = self.talk2dino.generate_masks(
+                    image_in,
+                    img_metas = None,
+                    text_emb = self.text_embed,
+                    classnames = ' '.join(self.ontology['labels']),
+                    apply_pamr = self.apply_pamr
+                )
 
-        mask_logits = masks * self.sharpness
+        mask_logits = masks.float() * self.sharpness
 
         img_out = mask_logits if self.return_logits else mask_logits.softmax(dim=1)
 
